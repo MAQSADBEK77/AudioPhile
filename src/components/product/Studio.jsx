d10211ab91a90fae88ed8e36e223bce8161e310e
Maqsadbek <110594640+MAQSADBEK77@users.noreply.github.com>
2023-08-21 03:48:28 +0500

    Studio flex to grid

--- src/components/product/Studio.jsx
+++ src/components/product/Studio.jsx
@@ -3,11 +3,13 @@ import React from "react";
 function Studio({img1, img2, img3}) {
   return (
     <div className="studio">
-      <div className="studio-left">
+      <div className="item1">
         <img src={img1} alt="" />
+      </div>
+      <div className="item5">
         <img src={img2} alt="" />
       </div>
-      <div className="studio-right">
+      <div className="item2">
         <img src={img3} alt="" />
       </div>
     </div>
