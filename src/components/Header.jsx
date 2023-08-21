c24433d8727c938810c97b640b9ff4917120be88
Maqsadbek <110594640+MAQSADBEK77@users.noreply.github.com>
2023-08-21 10:20:05 +0500

    See_button update not refresh site

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,7 +12,7 @@ function Header({ menu, setMenu }) {
         </div>
         <header className={menu && "header-active"}>
           <NavLink to="/" className={menu ? "logo logo-active" : "logo"}>
-            <svg onClick={() => setMenu(false)}>
+            <svg onClick={() => setMenu(false)}
               xmlns="http://www.w3.org/2000/svg" width="143" height="25"
               viewBox="0 0 143 25" fill="none">
               <path
--- src/components/See_button.jsx
+++ src/components/See_button.jsx
@@ -1,9 +1,16 @@
+import React from "react";
 import { Link } from "react-router-dom";
+
 function See_button({ to, clas }) {
+  const handleClick = (event) => {
+    window.scrollTo(0, 0);
+  };
+
   return (
-    <Link className={clas + " see_button"} to={to}>
+    <Link className={clas + " see_button"} to={to} onClick={handleClick}>
       See Product
     </Link>
   );
 }
+
 export default See_button;
