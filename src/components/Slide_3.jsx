94bdbe4cd1a0b2b4309e4d5b99e794d29984a58f
Maqsadbek <110594640+MAQSADBEK77@users.noreply.github.com>
2023-10-03 19:06:44 +0500

    Delete images n't bugs

--- src/components/Slide_3.jsx
+++ src/components/Slide_3.jsx
@@ -1,17 +1,16 @@
 import { Link } from "react-router-dom";
 import "./slide_3.css";
+import xx99 from "../img/xx99_1.png";
+import zx9 from "../img/zx9.png";
+import yx1 from "../img/yx1.png";
 function Slide_3() {
-  
   const handleClick = (event) => {
     window.scrollTo(0, 0);
   };
   return (
     <div className="slide">
       <div className="card-slide">
-        <img
-          className="img1"
-          src="https://s3-alpha-sig.figma.com/img/0d2e/6fc8/87d6513c34f10bac56e324f01ec77185?Expires=1693180800&Signature=W4w-sDgmPMkg2dVzcOpoqnVfKJWsMXeA1Lz-kyUoVlY-Ym5FPznQkwqjxU2tn9cZHPTdxzq8z8FlL3QLlDVF4lnO6KaXp~2Ww3RRpy1enTFYXiF6UAJqJ~AH47Vi-eGGCTQZWsFeQd-gbX5zJ6qhwzc9~yycq4a-q2ObF-IDA4VytTZMLUz5wwQkj3TrYcysPl2lObzUuvgGmvrjyj93z6uO4lZjOKIN8MFho2zspjLmDxeCXSBloDNOtQapVvysS2Wl1LnicDFXdfpoPUfzBA3OQyvoECIeJtAhQNHnmkjCzMqmf-H8O43i5rfJCpkQN55ORqSnWs01sePDGBpA~g__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
-        />
+        <img className="img1" src={xx99} />
         <h2>HEADPHONES</h2>
         <Link onClick={handleClick} to="/headphones">
           Shop{""}
@@ -30,10 +29,7 @@ function Slide_3() {
         </Link>
       </div>
       <div className="card-slide">
-        <img
-          className="img2"
-          src="https://s3-alpha-sig.figma.com/img/e0c8/6dd2/a7f4c7505f96ce15449ca178ce6b7ad1?Expires=1693180800&Signature=NVrTuLbdvTKh4dxwzoZPgq6YjnNLu8KVI9FAnDSBWPp0cwvTZeL0s2dFnWTi6F1e3GamTaBiqWtuokM~bWWh8U69sPIUX3lJLkha~KAJlGqCI7t1VM0n08CKVkDP9UViW7El5Un1ivxDp1sdLF09kyyZe0kfljAt94X~9823oD2xasXzNxg3yiBU7j-NRmUBk-~OPX871FNzSvaTcVc7aCy5xZLKDASGffU-rTclr22BayJssJActMtEFrzWKBjewhhfSF2qmCWIBUf1vrirRqlu-ID5j7Lq~yOf2YtX5bAGL0j0-Otr-HR-2vPDmlP1k7pF44Ot192oBpBQvzYUwA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
-        />
+        <img className="img2" src={zx9} />
         <h2>SPEAKERS</h2>
         <Link onClick={handleClick} to="/speakers">
           Shop{" "}
@@ -52,10 +48,7 @@ function Slide_3() {
         </Link>
       </div>
       <div className="card-slide">
-        <img
-          className="img3"
-          src="https://s3-alpha-sig.figma.com/img/d47b/304d/532a222f08c1500c16aa3ed52c16aa20?Expires=1693180800&Signature=md8yNfJaCuVRRRY30JWHfmDCEYbatriqbwSePONU~WSbKIG234YOQe8q2QywUTgOEaSD1rrtAY8ExUkNMfEuwg9otxCrIB3fqtvQ36pGrYoeYQwdFs1LN~7xUnyZMo-YRwHcjCe2n4o4c4HKae3wt9p83FeT5c4qc1OLBvH5eJPTsG57KeUN2nhw6-fk3wvaLJ73Thv9~c8X3L-Pb2qyiN4TNsbHaBIj61TLUuZ20J9YP~uPUIPm1BPbaAIilDM7P2NgiVMIjXIrp2W59UWDfEJw7P4oMKAs4pAX6mYhcmn7Z8NMdfOFn5yEumasJVnZV3HRpN9lsPVnloBKvS6O~w__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
-        />
+        <img className="img3" src={yx1} />
         <h2 className="three-h2">EARPHONES</h2>
         <Link onClick={handleClick} className="three-a" to="/earphones">
           Shop{" "}
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,6 +2,9 @@ import "./home.css";
 import See_button from "../../components/See_button";
 import Slide3 from "../../components/Slide_3";
 import zx7_img from "../../img/speaker-zx7.jpg";
+import zx9_img from "../../img/zx9.png";
+import earphome_img from "../../img/headphone-home.svg";
+
 import ManHeadphone from "../../components/ManHeadphone";
 function Home() {
   return (
@@ -21,7 +24,7 @@ function Home() {
         <Slide3 />
         <div className="zx9">
           <div className="zx9-left">
-            <img src="https://s3-alpha-sig.figma.com/img/e0c8/6dd2/a7f4c7505f96ce15449ca178ce6b7ad1?Expires=1693180800&Signature=NVrTuLbdvTKh4dxwzoZPgq6YjnNLu8KVI9FAnDSBWPp0cwvTZeL0s2dFnWTi6F1e3GamTaBiqWtuokM~bWWh8U69sPIUX3lJLkha~KAJlGqCI7t1VM0n08CKVkDP9UViW7El5Un1ivxDp1sdLF09kyyZe0kfljAt94X~9823oD2xasXzNxg3yiBU7j-NRmUBk-~OPX871FNzSvaTcVc7aCy5xZLKDASGffU-rTclr22BayJssJActMtEFrzWKBjewhhfSF2qmCWIBUf1vrirRqlu-ID5j7Lq~yOf2YtX5bAGL0j0-Otr-HR-2vPDmlP1k7pF44Ot192oBpBQvzYUwA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4" />
+            <img src={zx9_img} />
           </div>
           <div className="zx9-right">
             <h3>ZX9 SPEAKER</h3>
@@ -43,7 +46,7 @@ function Home() {
         </div>
         <div className="earthphone-home">
           <div className="earthphone-left">
-            <img src="https://s3-alpha-sig.figma.com/img/86bb/9423/fa199827480977a028c4dfc1f770ab02?Expires=1693180800&Signature=i8R8gIvFKIrBvw76L9PS1-T-UHzq475jGpE4EiSz2hG1FL9Sb73YWPiXSOfG9zmBdvBuGLmh8uMXrMW4KBjW93elWV4HOXujMJ7CPGGeTvz4dpZUvmFGnOZllJBGoBY7Ntk2OY1PZXM-ltnX2I84RvhY-4GuGzaGpS4i5H2VoNjOvqAGi5Hql3q6imV-DhJtG7qDlFan-QgbLWxMNm9P~lYQGmzeZ8-LtE5V9aBS3wWVlN877dmPTi3BggBvevIw2fZGkbW9VCb5rXH5-4hS1hWKLFhKDqFT5TLSbCSgS97F8UEitBTixMR~kQeo8L6VNXdLJUI11DSazP-Jp59VJw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4" />
+            <img cl src={earphome_img} />
           </div>
           <div className="earthphone-right">
             <div className="right-child">
